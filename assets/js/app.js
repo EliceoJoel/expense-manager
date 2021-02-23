b279b4d22197312624f73df1900f199316b3ad80
Elizeo <eliceoherbas@gmail.com>
2021-02-23 15:36:04 -0400

    proyect finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,7 @@ class Interfaz{
       surplusSpan.innerHTML = `$${amount}`;
    }
 
+   //print a message of error or success
    printMessage(textMessage, type){
       const message = document.querySelector(".message");
       message.innerHTML = textMessage
@@ -35,12 +36,48 @@ class Interfaz{
       }else{
          message.classList.remove("message-error");
          message.classList.add("message-success");
+         form.reset();
       }
       message.style.display = "block";
       setTimeout(()=>{
          message.style.display = "none";
-         if(type==="success"){form.reset();}
       }, 2000)
+   }
+
+   //Add the dudgets to list
+   addExpenseToList(name, amount){
+      const expenseList = document.querySelector(".expense-list");
+      const newExpense = `<li class="spending">
+         <p class="expense-name">${name}</p>
+         <p class="expense-amount">$${amount}</p>
+      </li>`
+      expenseList.innerHTML += newExpense;
+   }
+
+   //subtract the expense of the budget
+   remainingBudget(amount){
+      const surplusSpan = document.querySelector(".surplusAmount");
+      const userSurplusAmount = budgetAmount.subtractBudget(amount);
+      surplusSpan.innerHTML = `$${userSurplusAmount}`;
+      this.changeColorOfSurplus();
+   }
+
+   //change color of span label (amount/3)*2 = yellow   ,   (amount/3) = red
+   changeColorOfSurplus(){
+      const surplusSpan = document.querySelector(".surplus");
+      const surplus = budgetAmount.surplus;
+      const budget = budgetAmount.budget;
+      console.log(surplus, budget);
+      if(surplus < (budget*0.25)){
+         surplusSpan.classList.remove('surplus-25-50', 'surplus-50-75', 'surplus-75-100');
+         surplusSpan.classList.add('surplus-0-25');
+      }else if(surplus < (budget*0.5)){
+         surplusSpan.classList.remove('surplus-50-75', 'surplus-75-100');
+         surplusSpan.classList.add('surplus-25-50');
+      }else if(surplus < (budget*0.75)){
+         surplusSpan.classList.remove('surplus-75-100');
+         surplusSpan.classList.add('surplus-50-75');
+      }
    }
 }
 
@@ -68,6 +105,13 @@ form.addEventListener("submit", (e)=>{
    if(expenseName === "" || expenseAmount === ""){
       ui.printMessage("Empty fields", "error");
    }else{
-      ui.printMessage("Expense added correctly", "success");
+      //Verify that the remainder is greater than zero
+      if(expenseAmount <= budgetAmount.surplus){
+         ui.printMessage("Expense added correctly", "success");
+         ui.addExpenseToList(expenseName, expenseAmount);
+         ui.remainingBudget(expenseAmount);
+      }else{
+         ui.printMessage("Spending exceeds your remaining money", "error");
+      }
    }
 })
